f0d2b8d710417e0a15bc92fc1b949df9a4f5739f
LuizFelipePLO <felipe08074@gmail.com>
2023-04-10 14:53:05 -0300

    basic rest api update

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
 const express = require('express');
 
 // Constants
-const PORT = 3000;
+const port = 3000;
 const app = express();
 
 
 // App
-app.listen(PORT, () => {
-  console.log(`Running on http://localhost:${PORT}`);
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
 });
 
-// Endpoints
-app.get('/foo', (req, res) => {
-  res.json({"foo": "bar"});
+app.get('/users', (req, res) => {
+  // Logic to fetch all users from database
+  const users = [
+    { id: 1, name: 'John Doe', email: 'johndoe@example.com' },
+    { id: 2, name: 'Jane Doe', email: 'janedoe@example.com' }
+  ];
+  res.json(users);
 });
 
-app.use(express.urlencoded({
-  extended: true
-}));
-
-app.post('/bar', function(req, res) {
-    var body = req.body;
-    console.log(req.body.foo);
-    res.send(req.body.foo);
+app.get('/users/:id', (req, res) => {
+  // Logic to fetch a user with specific ID from database
+  const user = { id: req.params.id, name: 'John Doe', email: 'johndoe@example.com' };
+  res.json(user);
 });
 
+app.post('/users', (req, res) => {
+  // Logic to create a new user in the database
+  const user = { name: req.body.name, email: req.body.email };
+  res.json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+  // Logic to update a user with specific ID in the database
+  const user = { id: req.params.id, name: req.body.name, email: req.body.email };
+  res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+  // Logic to delete a user with specific ID from the database
+  res.send(`User with ID ${req.params.id} deleted successfully`);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
