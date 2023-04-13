ae6ee4bde702a28bcde7df7962148507d2050265
bruno <brunoramos013@gmail.com>
2023-04-12 21:21:42 -0300

    insert function refresh token

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -3,9 +3,6 @@ const request = require('request');
 require('dotenv').config();
 const config = require('../config/config');
 
-const Keycloak = require('keycloak-connect');
-const keycloak = new Keycloak({}, config);
-
 function login(req, res) {
     const options = {
         url: `${config.baseApiUrl}/realms/${config.realm}/protocol/openid-connect/token`,
@@ -25,6 +22,7 @@ function login(req, res) {
         try {
             const data = JSON.parse(body);
             if (data.access_token) {
+                req.session.refresh_token = data.refresh_token;
                 return res.status(200).send({
                     message: 'Successfully Generated Token',
                     token_type: data.token_type,
@@ -44,7 +42,7 @@ function login(req, res) {
 }
 
 function refreshTokens(req, res) {
-    const refreshToken = localStorage.getItem('refresh_token');
+    const refreshToken = req.session.refresh_token;
     const options = {
         url: `${config.baseApiUrl}/realms/${config.realm}/protocol/openid-connect/token`,
         form: {
@@ -55,7 +53,6 @@ function refreshTokens(req, res) {
         },
     };
 
-    console.log('refreshTokens' + refreshToken);
     request.post(options, (error, response, body) => {
         if (error) {
             console.error(error);
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,20 @@
 const request = require('request');
 const express = require('express');
+const session = require('express-session');
 const routes = express.Router();
-
 const LoginController = require('./controllers/LoginController');
 const UserController = require('./controllers/UserController');
 
+const memoryStore = new session.MemoryStore();
+routes.use(session({
+    secret: 'seu-secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+
 routes.post('/login', LoginController.login);
 routes.post('/refreshToken', LoginController.refreshTokens);
-
 routes.post('/users', UserController.createUser);
 routes.get('/users', UserController.listUsers);
 routes.get('/users/:id', UserController.listUserById);
